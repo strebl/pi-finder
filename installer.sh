962769c4e9bc09453f3b675a251038150dec5585
Manuel Strebel <manuel_st@msn.com>
2015-04-02 02:22:58 +0200

    create installer

---
+++ installer.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Update sources
+#apt-get update
+
+# Install Node.js
+echo "Installing Node.js"
+#apt-get install -y nodejs
+
+
+# Create symlink if needed
+if [ -e /usr/bin/nodejs && ! -e /usr/bin/node ]
+  then
+  	echo "Creating symlink"
+    #ln -s /usr/bin/nodejs /usr/bin/node
+fi
+
+# Install pi-finder
+echo "Installing pi-finder with npm"
+#npm install -g https://github.com/strebl/pi-finder/tarball/master
+
+# Get the paths 	# To extact the node path: nodepath=$(npm config get prefix)
+packagepath="/usr/local/lib/node_modules/pi-finder"
+
+# Move to init.d
+echo "Moving init script"
+#mv "$packagepath/init/pi-finder /etc/init.d/"
+
+# Change permissions
+echo "Chaning init script permissions"
+#chmod 755 /etc/init.d/pi-finder
+
+# Update rc.d
+echo "Updating rc.d"
+#update-rc.d pi-finder defaults
