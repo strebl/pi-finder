aec72a5007cacb1c1282806d5fb7ec1067b59c27
Manuel Strebel <manuel_st@msn.com>
2015-05-28 19:21:48 +0200

    protect the user from download stream interruptions.

--- installer.sh
+++ installer.sh
@@ -1,61 +1,63 @@
 #!/usr/bin/env bash
 
-# Declare colors
-RS="\033[0m"    # reset
-FGRD="\033[01;31m" # foreground red
-FGRN="\033[01;32m" # foreground green
-FBLE="\033[01;34m" # foreground blue
+{
+	# Declare colors
+	RS="\033[0m"    # reset
+	FGRD="\033[01;31m" # foreground red
+	FGRN="\033[01;32m" # foreground green
+	FBLE="\033[01;34m" # foreground blue
 
-# Make sure only root can run our script
-if [[ $EUID -ne 0 ]]; then
-	echo -e "${FGRD}The pi-finder installer must be run as root!${RS}" 1>&2
-	exit 1
-fi
+	# Make sure only root can run our script
+	if [[ $EUID -ne 0 ]]; then
+		echo -e "${FGRD}The pi-finder installer must be run as root!${RS}" 1>&2
+		exit 1
+	fi
 
-# Update sources
-apt-get update
+	# Update sources
+	apt-get update
 
-# Install curl
-echo "Installing curl"
-apt-get install curl
+	# Install curl
+	echo "Installing curl"
+	apt-get install curl
 
-# Add Node.js sources
-echo "Adding Node.js sources"
-curl -sL https://deb.nodesource.com/setup | bash -
+	# Add Node.js sources
+	echo "Adding Node.js sources"
+	curl -sL https://deb.nodesource.com/setup | bash -
 
-# Install Node.js
-echo "Installing Node.js"
-apt-get install -y nodejs
+	# Install Node.js
+	echo "Installing Node.js"
+	apt-get install -y nodejs
 
 
-# Create symlink if needed
-if [ -e /usr/bin/nodejs ] && [ ! -e /usr/bin/node ]
-  then
-  	echo "Creating symlink"
-    ln -s /usr/bin/nodejs /usr/bin/node
-fi
+	# Create symlink if needed
+	if [ -e /usr/bin/nodejs ] && [ ! -e /usr/bin/node ]
+	  then
+	  	echo "Creating symlink"
+	    ln -s /usr/bin/nodejs /usr/bin/node
+	fi
 
-# Install pi-finder
-echo "Installing pi-finder with npm"
-npm install -g https://github.com/strebl/pi-finder/tarball/master
+	# Install pi-finder
+	echo "Installing pi-finder with npm"
+	npm install -g https://github.com/strebl/pi-finder/tarball/master
 
-# Get the paths
-nodejsdir=$(npm config get prefix)
-packagepath="$nodejsdir/lib/node_modules/pi-finder"
+	# Get the paths
+	nodejsdir=$(npm config get prefix)
+	packagepath="$nodejsdir/lib/node_modules/pi-finder"
 
-# Move to init.d
-echo "Moving init script"
-mv $packagepath/init/pi-finder /etc/init.d/
+	# Move to init.d
+	echo "Moving init script"
+	mv $packagepath/init/pi-finder /etc/init.d/
 
-# Change permissions
-echo "Chaning init script permissions"
-chmod 755 /etc/init.d/pi-finder
+	# Change permissions
+	echo "Chaning init script permissions"
+	chmod 755 /etc/init.d/pi-finder
 
-# Update rc.d
-echo "Updating rc.d"
-update-rc.d pi-finder defaults
+	# Update rc.d
+	echo "Updating rc.d"
+	update-rc.d pi-finder defaults
 
-echo
-echo "${FBLE}Before you continue, change the name attribute in the config!${RS}"
-echo "${FGRN}Run sudo nano $packagepath/config.js${RS}"
-echo
+	echo
+	echo -e "${FBLE}Before you continue, change the name attribute in the config!${RS}"
+	echo -e "${FGRN}Run sudo nano $packagepath/config.js${RS}"
+	echo
+}
