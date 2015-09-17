7098de1fefad414cbdad2cbd435408aa49618298
Manuel Strebel <manuel.strebel@gibb.ch>
2015-09-17 18:05:26 +0200

    new node installation an service user

--- installer.sh
+++ installer.sh
@@ -1,40 +1,46 @@
 #!/usr/bin/env bash
 
 {
-	# Declare colors
+	# Use the form until the main repo fixed issue #3
+	# https://github.com/coolaj86/iojs-install-script/issues/3
+	NODEJS_INSTALLER_URL="https://raw.githubusercontent.com/strebl/iojs-install-script/fix-3/setup-min.sh"
+	#NODEJS_INSTALLER_URL="https://raw.githubusercontent.com/coolaj86/iojs-install-script/master/setup-min.sh"
+
+	##################
+	# Declare colors #
+	##################
 	RS="\033[0m"		# reset
 	FGRD="\033[01;31m"	# foreground red
 	FGRN="\033[01;32m"	# foreground green
 	FBLE="\033[01;34m"	# foreground blue
 
-	# Make sure only root can run our script
+	#########################
+	# Check for root rights #
+	#########################
 	if [[ $EUID -ne 0 ]]; then
 		echo -e "${FGRD}The pi-finder installer must be run as root!${RS}" 1>&2
 		exit 1
 	fi
 
-	# Update sources
-	apt-get update
-
-	# Install curl
-	echo "Installing curl"
-	apt-get install curl
-
-	# Add Node.js sources
-	echo "Adding Node.js sources"
-	curl -sL https://deb.nodesource.com/setup | bash -
-
-	# Install Node.js
-	echo "Installing Node.js"
-	apt-get install -y nodejs
-
-
-	# Create symlink if needed
-	if [ -e /usr/bin/nodejs ] && [ ! -e /usr/bin/node ]
-	  then
-	  	echo "Creating symlink"
-	    ln -s /usr/bin/nodejs /usr/bin/node
+	##################################
+	# Download the Node.js installer #
+	# Copyright github.com/coolaj86  #
+	##################################
+	if [ -n "$(which curl)" ]; then
+		curl --silent "${NODEJS_INSTALLER_URL}" \
+			-o /tmp/install-nodejs.bash || echo 'error downloading Node.js setup script'
+	elif [ -n "$(which wget)" ]; then
+		wget --quiet "${NODEJS_INSTALLER_URL}" \
+			-O /tmp/install-nodejs.bash || echo 'error downloading Node.js setup script'
+	else
+		echo "Found neither 'curl' nor 'wget'. Can't Continue."
+		exit 1
 	fi
+
+	####################
+	# Install Node.js  #
+	####################
+	bash /tmp/install-nodejs.bash
 
 	# Install pi-finder
 	echo "Installing pi-finder with npm"
@@ -55,6 +61,9 @@
 	# Update rc.d
 	echo "Updating rc.d"
 	update-rc.d pi-finder defaults
+
+	# Create service user
+	id -u "pi-finder" &>/dev/null || useradd -r -s /bin/false pi-finder
 
 	echo
 	echo -e "${FBLE}Before you continue, change the name attribute in the config!${RS}"
